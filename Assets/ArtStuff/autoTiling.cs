d565ea77a6fba82e9242b8daf85b40cf5e515b11
Mehael <mehael@mail.ru>
2017-04-23 14:14:03 +1000

    Add Water and Clouds

---
+++ Assets/ArtStuff/autoTiling.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class autoTiling : MonoBehaviour
+{
+    public float scrollSpeed = 0.5F;
+    new Renderer renderer;
+    void Start() {
+        renderer = GetComponent<Renderer>();
+    }
+    void Update() {
+        float offset = Time.time * scrollSpeed;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(offset, -0.01f));
+    }
+}
